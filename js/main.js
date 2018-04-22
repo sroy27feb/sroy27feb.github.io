43ea99960ac2532a1617647397f792a775b11155
sroy27feb <sroy27feb@gmail.com>
2018-04-22 11:49:32 +0530

    shop

---
+++ js/main.js
@@ -0,0 +1,400 @@
+function changePrice(x)
+{
+
+	var price=document.getElementById('price');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice1(x)
+{
+
+	var price=document.getElementById('price1');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice2(x)
+{
+
+	var price=document.getElementById('price2');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice3(x)
+{
+
+	var price=document.getElementById('price3');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice4(x)
+{
+
+	var price=document.getElementById('price4');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice5(x)
+{
+
+	var price=document.getElementById('price5');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice6(x)
+{
+
+	var price=document.getElementById('price6');
+	var p=(x.value*price.value)/1000;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice7(x)
+{
+
+	var price=document.getElementById('price7');
+	var p=(x.value*price.value)/1000;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice8(x)
+{
+
+	var price=document.getElementById('price8');
+	var p=(x.value*price.value)/1000;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice9(x)
+{
+
+	var price=document.getElementById('price9');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice10(x)
+{
+
+	var price=document.getElementById('price10');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice11(x)
+{
+
+	var price=document.getElementById('price11');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice12(x)
+{
+
+	var price=document.getElementById('price12');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice13(x)
+{
+
+	var price=document.getElementById('price13');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice14(x)
+{
+
+	var price=document.getElementById('price14');
+	var p=(x.value*price.value)/200;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice15(x)
+{
+
+	var price=document.getElementById('price15');
+	var p=(x.value*price.value)/200;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice16(x)
+{
+
+	var price=document.getElementById('price16');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice17(x)
+{
+
+	var price=document.getElementById('price17');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice18(x)
+{
+
+	var price=document.getElementById('price18');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice19(x)
+{
+
+	var price=document.getElementById('price19');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice20(x)
+{
+
+	var price=document.getElementById('price20');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice21(x)
+{
+
+	var price=document.getElementById('price21');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice22(x)
+{
+
+	var price=document.getElementById('price22');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice23(x)
+{
+
+	var price=document.getElementById('price23');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice24(x)
+{
+
+	var price=document.getElementById('price24');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice25(x)
+{
+
+	var price=document.getElementById('price25');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice26(x)
+{
+
+	var price=document.getElementById('price26');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice27(x)
+{
+
+	var price=document.getElementById('price27');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice28(x)
+{
+
+	var price=document.getElementById('price28');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice29(x)
+{
+
+	var price=document.getElementById('price29');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice30(x)
+{
+
+	var price=document.getElementById('price30');
+	var p=(x.value*price.value);
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice31(x)
+{
+
+	var price=document.getElementById('price31');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice32(x)
+{
+
+	var price=document.getElementById('price32');
+	var p=(x.value*price.value)/100;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice33(x)
+{
+
+	var price=document.getElementById('price33');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice34(x)
+{
+
+	var price=document.getElementById('price34');
+	var p=(x.value*price.value);
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice35(x)
+{
+
+	var price=document.getElementById('price35');
+	var p=(x.value*price.value);
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice36(x)
+{
+
+	var price=document.getElementById('price36');
+	var p=(x.value*price.value);
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice37(x)
+{
+
+	var price=document.getElementById('price37');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice38(x)
+{
+
+	var price=document.getElementById('price38');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice39(x)
+{
+
+	var price=document.getElementById('price39');
+	var p=(x.value*price.value);
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice40(x)
+{
+
+	var price=document.getElementById('price40');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice41(x)
+{
+
+	var price=document.getElementById('price41');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice42(x)
+{
+
+	var price=document.getElementById('price42');
+	var p=(x.value*price.value);
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice43(x)
+{
+
+	var price=document.getElementById('price43');
+	var p=(x.value*price.value);
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice44(x)
+{
+
+	var price=document.getElementById('price44');
+	var p=(x.value*price.value);
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice45(x)
+{
+
+	var price=document.getElementById('price45');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice47(x)
+{
+
+	var price=document.getElementById('price47');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice48(x)
+{
+
+	var price=document.getElementById('price48');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice49(x)
+{
+
+	var price=document.getElementById('price49');
+	var p=(x.value*price.value);
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
+function changePrice50(x)
+{
+
+	var price=document.getElementById('price50');
+	var p=(x.value*price.value)/250;
+	console.log(p);
+	price.innerHTML='₹ '+p+'.00';
+}
